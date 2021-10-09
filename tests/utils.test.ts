53567045ff939a3876f51b36c09aa48c0093ffa5
Mahmoud Zohdi <mahmoud.zohdi@gmail.com>
2021-10-09 10:07:07 +0200

    add test cases for generateInputInstructionsObject util

---
+++ tests/utils.test.ts
@@ -0,0 +1,59 @@
+import assert from "assert";
+import { InstractionsInterface } from "../app/types";
+import { generateInputInstructionsObject } from "../app/utils";
+
+describe("test generateInputInstructionsObject util", function () {
+  it("[happy senarios] should return the correct instractions object from string", function () {
+    const expectedResults: InstractionsInterface = {
+      plateau: { x: 5, y: 5 },
+      inputs: {
+        Rover1: {
+          landing: { x: 1, y: 2, orientation: "N" },
+          instractions: "LMLMLMLMM",
+        },
+        Rover2: {
+          landing: { x: 3, y: 3, orientation: "E" },
+          instractions: "MMRMMRMRRM",
+        },
+      },
+    };
+    const instractionsObject = generateInputInstructionsObject(`
+Plateau:5 5
+Rover1 Landing:1 2 N
+Rover1 Instructions:LMLMLMLMM
+Rover2 Landing:3 3 E
+Rover2 Instructions:MMRMMRMRRM
+    `);
+
+    assert.deepEqual(instractionsObject, expectedResults);
+  });
+  it("[bad senarios] should avoid empty lines and not depend on instructions sequence", () => {
+    const expectedResults: InstractionsInterface = {
+      plateau: { x: 5, y: 5 },
+      inputs: {
+        Rover1: {
+          landing: { x: 1, y: 2, orientation: "N" },
+          instractions: "LMLMLMLMM",
+        },
+        Rover2: {
+          landing: { x: 3, y: 3, orientation: "E" },
+          instractions: "MMRMMRMRRM",
+        },
+      },
+    };
+    const instractionsObject = generateInputInstructionsObject(`
+
+    Plateau:5 5
+
+    Rover1 Instructions:LMLMLMLMM
+
+    Rover2 Landing:3 3 E
+
+Rover2 Instructions:MMRMMRMRRM
+
+Rover1 Landing:1 2 N
+    `);
+
+    assert.deepEqual(instractionsObject, expectedResults);
+  });
+});
